cc3677d25c340f3a90fd9f91e43b544179495d4a
naveen-ku <naveen2951996@gmail.com>
2019-10-31 18:53:14 +0530

    Adding user account and Profile picture.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField , SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -35,3 +37,25 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username",
+                            validators=[DataRequired(), Length(min=6, max=20)]
+                          )
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()]
+                        )
+    picture = FileField('Update Profile Pictures', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('That username already exists. Please choose a different one.')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email = email.data).first()
+            if email:
+                raise ValidationError('That email already exists. Please choose a different one.')
